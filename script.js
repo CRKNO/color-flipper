571a1f9de44391f0536da0fdd857f301dc952b6e
CRKNO <cristiancanosantiago@hotmail.com>
2023-05-11 22:05:19 -0300

    added functionality to button to change background color

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]; //Hexadecimal characters.
 
+const colorSpan = document.querySelector("span");
+const bodyElement = document.querySelector("body");
+const flipperButton = document.querySelector("button");
 
-function getHexCode(){
+function colorFlip(){
     let hexCode = "#";
 
     for(let i = 0; i < 6; i++){ 
         let char = Math.floor(Math.random()*(16)); //Random number between 0 and 15.
         hexCode = hexCode + hex[char]; // Concatenates the characters.
     }
-    console.log(hexCode);
+    colorSpan.innerHTML = hexCode;
+    bodyElement.style.backgroundColor = hexCode;
 }
+
+flipperButton.addEventListener("click", ()=>{
+    colorFlip();
+});
+
